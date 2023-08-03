eae75c51e3e660b1cb3d4e2a9f4c03075aeff2ca
Krishab Bashyal <krishab2002@gmail.com>
2023-08-03 18:06:47 -0500

    setup of a supabase component for testing

---
+++ frontend/src/components/Supabase.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="status in supabaseStatus" :key="status.id">Supabase: {{ status.status }}</li>
+  </ul>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { supabase } from "../lib/supabaseClient";
+
+const supabaseStatus = ref([]);
+
+async function checkStatus() {
+  const { data } = await supabase.from("supabaseStatus").select();
+  supabaseStatus.value = data;
+}
+
+onMounted(() => {
+   checkStatus();
+});
+</script>
