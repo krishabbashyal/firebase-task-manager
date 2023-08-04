a3dd01185ccfa4e97159469c0a19c030063be671
Krishab <krishabbashyal@gmail.com>
2023-08-03 21:45:33 -0500

    created a new workspace to develop new components

---
+++ frontend/src/components/ComponentWorkspace.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>This is the webpage that will be used to view and develop components</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponentWorkspace",
+};
+</script>
