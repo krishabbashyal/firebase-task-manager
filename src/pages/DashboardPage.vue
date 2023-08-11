18a425064f95476b5b53e1cc8ec3a6a8489a3a5f
Krishab <krishab2002@gmail.com>
2023-08-10 22:34:48 -0500

    started protecting routes and added sign out

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
-    <section-header class="mt-16 mb-1"/>
-    <card-scroller/>
+    <section-header class="mt-16 mb-1" />
+    <card-scroller />
+      <div class="flex flex-row justify-center items-center">
+        <button
+          @click="signOut"
+          class="mt-10 h-14 w-80 bg-dark-gray font-medium text-white rounded-lg"
+        >
+          Sign Out
+        </button>
+      </div>
   </div>
 </template>
 
 <script>
-import SectionHeader from '@/components/SectionHeader.vue';
-import CardScroller from '@/components/CardScroller.vue';
+
+import SectionHeader from "@/components/SectionHeader.vue";
+import CardScroller from "@/components/CardScroller.vue";
+import { supabase } from "../lib/supabaseClient.js";
 
 export default {
   components: {
     SectionHeader,
-    CardScroller
+    CardScroller,
+  },
+  methods: {
+    async signOut() {
+      const { error } = await supabase.auth.signOut();
+      console.log(error)
+      window.location.reload();
+    },
   },
   name: "DashboardPage",
 };
