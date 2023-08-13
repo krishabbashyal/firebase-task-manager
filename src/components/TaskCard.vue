030da2f9a11b372edc7faca13073035d70d1b827
Krishab <krishab2002@gmail.com>
2023-08-12 23:43:41 -0500

    trying out line clamp

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="bg-white w-full h-[5.625rem] rounded-3xl flex-shrink-0 text-ellipsis overflow-hidden">
+  <div class="bg-white w-full h-[5.625rem] rounded-3xl flex-shrink-0">
     <div class="px-[17px]">
       <p class="text-sm pt-3">JourneyJot</p>
-      <p class="leading-5 pt-1">Suppress email notifications for users who have decided to opt out of receiving emails.</p>
+      <p class="leading-5 pt-1 line-clamp-2">Suppress email notifications for users who have decided to opt out of receiving emails. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas nihil mollitia reprehenderit maiores id, ex sint deleniti excepturi fuga esse at quaerat optio ab facilis, voluptate est quia harum odio?</p>
     </div>
   </div>
 </template>
